6e785654583811379ab613fa47bf37d061526dc5
banseokSuh <still3028@gmail.com>
2021-06-28 22:08:39 +0900

    [Server] feat/routing edit: user.controller.ts, user.service.ts

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+
+	constructor(private readonly userService: UserService) { }
+
+	// 랭킹: 사용자
+	@Get('rank')
+	getRank() {
+		return this.userService.getRank();
+	}
+
+	// 로그인
+	@Post('signin')
+	signIn() {
+		return this.userService.signIn();
+	}
+
+	// 로그아웃
+	@Post('signout')
+	signOut() {
+		return this.userService.signOut();
+	}
+
+	// 회원가입
+	@Post('signup')
+	signUp() {
+		return this.userService.signUp();
+	}
+
+}
