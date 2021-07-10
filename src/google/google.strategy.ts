e4be6c75d4ae6cbc9065eec51e0ecf6cbd76a875
banseokSuh <still3028@gmail.com>
2021-07-10 21:36:34 +0900

    [Server] Dev : modified googleService v.05

--- src/google/google.strategy.ts
+++ src/google/google.strategy.ts
@@ -6,12 +6,15 @@ import { Injectable } from '@nestjs/common';
 
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
 	constructor() {
+		
+		// http://ec2-52-79-247-245.ap-northeast-2.compute.amazonaws.com
+		const url = 'https://api.liteseoul.com';
 		super({
 			clientID: process.env.GOOGLE_CLIENT_ID,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-			// callbackURL: 'http://localhost:80/google/auth/google/callback',
+			callbackURL: `${url}/google/auth/google/callback`,
 			// callbackURL: 'https://api.liteseoul.com/google/auth/google/callback',
-			callbackURL: 'http://ec2-52-79-247-245.ap-northeast-2.compute.amazonaws.com/google/auth/google/callback',
+			// callbackURL: 'http://ec2-52-79-247-245.ap-northeast-2.compute.amazonaws.com/google/auth/google/callback',
 			scope: ['email', 'profile']
 		});
 	}
